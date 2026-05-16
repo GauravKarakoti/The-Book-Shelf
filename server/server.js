78be2fbcee792942f83f5c25096fb02d4590bdf8
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-05-16 20:31:58 +0530

    Using RegEx

--- server/server.js
+++ server/server.js
@@ -27,11 +27,12 @@ app.use(express.static('client/build'));
 
 if(process.env.NODE_ENV === 'production') {
     const path = require('path');
-    app.get('*', (req, res) => {
+    app.get(/.*/, (req, res) => {
         console.log('works');
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     })
 }
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
